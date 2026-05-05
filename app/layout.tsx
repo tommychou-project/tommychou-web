e30a283adbc59d97378fbfbc80cf38a7e4862b72
Tommy Chou <tommy.chou28@gmail.com>
2026-05-05 15:00:30 +0800

    add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-88VQMPMJDP"></script>
+  <script dangerouslySetInnerHTML={{ __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-88VQMPMJDP');
+  `}} />
+  {children}
+</body>
     </html>
   );
 }
